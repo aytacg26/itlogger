65c7c3128da9891f12840240cab52d71d4548aae
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-03-16 18:46:36 +0200

    Logs and SearchBar added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import React, { useEffect, Fragment } from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
+import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
 
-function App() {
-  return <div className='App'>React App</div>;
-}
+const App = () => {
+  useEffect(() => {
+    //Initializes Materialize JS
+    M.AutoInit();
+  }, []);
+
+  return (
+    <Fragment>
+      <SearchBar />
+      <div className='container'>
+        <Logs />
+      </div>
+    </Fragment>
+  );
+};
 
 export default App;
