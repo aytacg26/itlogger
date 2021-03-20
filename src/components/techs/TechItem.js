00da1992b7e91535da32d93d2527ed09636d3e7f
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-03-20 21:24:51 +0200

    deleteTech action of redux used

--- src/components/techs/TechItem.js
+++ src/components/techs/TechItem.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { deleteTech } from '../../actions/techActions';
+import M from 'materialize-css/dist/js/materialize.min.js';
 
-const TechItem = ({ tech }) => {
+const TechItem = ({ tech, deleteTech }) => {
+  const handleDelete = () => {
+    deleteTech(tech.id);
+    M.toast({
+      html: `${tech.firstname} ${tech.lastname} deleted from technicians list successfully...`,
+    });
+  };
+
   return (
     <li className='collection-item'>
       <div>
         {tech.firstname} {tech.lastname}
-        <a href='#!' className='secondary-content'>
+        <a href='#!' className='secondary-content' onClick={handleDelete}>
           <i className='material-icons grey-text'>delete</i>
         </a>
       </div>
@@ -18,4 +28,8 @@ TechItem.propTypes = {
   tech: PropTypes.object.isRequired,
 };
 
-export default TechItem;
+const mapDispatchToProps = {
+  deleteTech,
+};
+
+export default connect(null, mapDispatchToProps)(TechItem);
