859b5ced95b8b6957b0ce5b54d516d123c3fa968
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-03-20 21:23:55 +0200

    addTech and deleteTech Actions added

--- src/actions/techActions.js
+++ src/actions/techActions.js
@@ -25,6 +25,47 @@ export const getTechs = () => async (dispatch) => {
   }
 };
 
+export const addTech = (tech) => async (dispatch) => {
+  try {
+    setLoading();
+    const res = await fetch('/techs', {
+      method: 'POST',
+      body: JSON.stringify(tech),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const data = await res.json();
+
+    dispatch({
+      type: ADD_TECH,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: TECHS_ERROR,
+      payload: error.response.statusText,
+    });
+  }
+};
+
+export const deleteTech = (id) => async (dispatch) => {
+  try {
+    setLoading();
+    await fetch(`/techs/${id}`);
+
+    dispatch({
+      type: DELETE_TECH,
+      payload: id,
+    });
+  } catch (error) {
+    dispatch({
+      type: TECHS_ERROR,
+      payload: error.response.statusText,
+    });
+  }
+};
+
 //SET LOADING TO TRUE
 export const setLoading = () => {
   return {
