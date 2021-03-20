17ec758d39153a2ffdcf67fa28018d2b23afd2a5
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-03-20 20:07:13 +0200

    techReducer created

---
+++ src/reducers/techReducer.js
@@ -0,0 +1,54 @@
+import {
+  GET_TECHS,
+  ADD_TECH,
+  DELETE_TECH,
+  TECHS_ERROR,
+  SET_LOADING,
+} from '../actions/types';
+
+const initialState = {
+  techs: null,
+  error: null,
+  loading: false,
+};
+
+const techReducer = (state = initialState, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case GET_TECHS:
+      return {
+        ...state,
+        techs: payload,
+        loading: false,
+      };
+    case ADD_TECH:
+      return {
+        ...state,
+        techs: [...state, payload],
+      };
+    case DELETE_TECH:
+      return {
+        ...state,
+        techs: state.techs.filter((tech) => tech.id !== payload),
+      };
+
+    case SET_LOADING:
+      return {
+        ...state,
+        loading: true,
+      };
+
+    case TECHS_ERROR:
+      return {
+        ...state,
+        error: payload,
+        loading: false,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default techReducer;
