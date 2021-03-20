35f0926d2e8b0cdf68b89b3da9c724691fbb88e7
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-03-20 18:21:01 +0200

    Comp. connected to the redux for searchLog func.

--- src/components/layout/SearchBar.js
+++ src/components/layout/SearchBar.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { connect } from 'react-redux';
+import { searchLogs } from '../../actions/logActions';
 
-const SearchBar = () => {
+const SearchBar = ({ searchLogs }) => {
+  const text = useRef('');
+
+  const handleSearch = () => {
+    searchLogs(text.current.value);
+  };
+
   return (
     <nav style={{ marginBottom: '30px' }} className='blue'>
       <div className='nav-wrapper'>
         <form>
           <div className='input-field'>
-            <input type='search' id='search' />
+            <input
+              type='search'
+              id='search'
+              ref={text}
+              onChange={handleSearch}
+            />
             <label className='label-icon' htmlFor='search'>
               <i className='material-icons'>search</i>
             </label>
@@ -18,4 +31,8 @@ const SearchBar = () => {
   );
 };
 
-export default SearchBar;
+const mapDispatchToProps = {
+  searchLogs,
+};
+
+export default connect(null, mapDispatchToProps)(SearchBar);
