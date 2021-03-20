12333ba370a430ca0ae68d7d28e6cfe71f7bc2da
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-03-20 16:29:10 +0200

    redux getLog func. used to fetch logs

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,27 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import Preloader from '../layout/Preloader';
 import LogItem from './LogItem';
+import { connect } from 'react-redux';
+import { getLogs } from '../../actions/logActions';
 
-const Logs = () => {
-  const [logs, setLogs] = useState([]);
-  const [loading, setLoading] = useState(false);
-
+const Logs = ({ logs, loading, error, getLogs }) => {
   useEffect(() => {
     getLogs();
 
     //eslint-disable-next-line
   }, []);
 
-  const getLogs = async () => {
-    setLoading(true);
-    const res = await fetch('/logs');
-    const data = await res.json();
-
-    setLogs(data);
-    setLoading(false);
-  };
-
-  if (loading) {
+  if (loading || logs === null) {
     return <Preloader />;
   }
 
@@ -39,4 +29,13 @@ const Logs = () => {
   );
 };
 
-export default Logs;
+const mapStateToProps = (state) => ({
+  logs: state.log.logs,
+  loading: state.log.loading,
+  error: state.log.error,
+});
+
+const mapDispatchToProps = {
+  getLogs,
+};
+export default connect(mapStateToProps, mapDispatchToProps)(Logs);
