d06a19c534f2e4b147d7901f0bd3e607ac7584ac
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-03-20 20:08:06 +0200

    getTechs and setLoading created for TechActions

---
+++ src/actions/techActions.js
@@ -0,0 +1,33 @@
+import {
+  GET_TECHS,
+  ADD_TECH,
+  DELETE_TECH,
+  TECHS_ERROR,
+  SET_LOADING,
+} from './types';
+
+//Get Techs
+export const getTechs = () => async (dispatch) => {
+  try {
+    setLoading();
+    const res = await fetch('/techs');
+    const data = await res.json();
+
+    dispatch({
+      type: GET_TECHS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: TECHS_ERROR,
+      payload: error.response.statusText,
+    });
+  }
+};
+
+//SET LOADING TO TRUE
+export const setLoading = () => {
+  return {
+    type: SET_LOADING,
+  };
+};
